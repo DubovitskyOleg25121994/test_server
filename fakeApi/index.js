7b3528a3ce56d95e7ebb8e99d72f8da6339c7032
oleg dubovitsky <dubovitsky@mail.ru>
2020-01-28 16:23:11 +0300

    removed rabbitQM, renamed fakeApi to api, added .gitignore, removed node_modules

--- fakeApi/index.js
+++ fakeApi/index.js
@@ -5,6 +5,11 @@ const normalizedPath = [
   "https://i.ibb.co/ng5R1Nq/1005-P0003.jpg",
   "https://i.ibb.co/vDwYL8k/1011-P0001.jpg",
   "https://i.ibb.co/R2Rsdtj/1012-P0002.jpg",
+  "https://i.ibb.co/W0VVcVp/1013-P0001.jpg",
+  "https://i.ibb.co/smGTWh1/100-P0001.jpg",
+  "https://i.ibb.co/ng5R1Nq/1005-P0003.jpg",
+  "https://i.ibb.co/vDwYL8k/1011-P0001.jpg",
+  "https://i.ibb.co/R2Rsdtj/1012-P0002.jpg",
   "https://i.ibb.co/W0VVcVp/1013-P0001.jpg"
 ];
   console.log('normalizedPath', normalizedPath); 
